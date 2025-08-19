11da5570d8cf86ef84d63d1daa8c30337b2f47d6
jhay-25 <jhay-25@users.noreply.github.com>
2025-08-19 08:11:15 +0000

    Deploy preview for PR 2 🛫

---
+++ pr/pr-2/entry-preview-BatSasO0.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-BJuS0OWq.js';
+import './index-Kjm4kNkQ.js';
+import './lit-html-8IiUFkrl.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
