8856c99f42e6d635e78f14ba30ca7e1677c5850a
megheaiulian <iulian@plumelo.com>
2025-08-27 11:34:10 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-rating@a14e0eb9d18e9872845aacbfd079c42e282a4a72 🚀

---
+++ entry-preview-c-WJnWAy.js
@@ -0,0 +1,7 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-BsKBBnWo.js';
+import './index-Kjm4kNkQ.js';
+import './lit-html-CedCbRno.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
